7f6ded49b0432592a93af0de83085232800d08fe
skiran017 <skiran017@gmail.com>
2023-07-06 13:40:29 +0530

    feat: add basic AmountCard Component

---
+++ src/components/AmountCard/AmountCard.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Flex, Text, Box } from '@chakra-ui/react';
+import { daysLeft } from '../../utils/helpers';
+
+function AmountCard({
+  owner,
+  title,
+  description,
+  target,
+  deadline,
+  amountCollected,
+  image,
+  handleClick,
+}) {
+  return <div>AmountCard</div>;
+}
+
+export default AmountCard;
