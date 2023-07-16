418a7de8908aedc533c2e17a6dad87ec170b4d2b
skiran017 <skiran017@gmail.com>
2023-07-17 03:27:33 +0530

    feat: add test cases for CrowdFunding smart contract
    -added required packages

--- web3-core/hardhat.config.js
+++ web3-core/hardhat.config.js
@@ -1,24 +1,27 @@
 /** @type import('hardhat/config').HardhatUserConfig */
+const { task } = require('hardhat/config');
+
+require('@nomiclabs/hardhat-ethers');
+require('dotenv').config();
+
+task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
 module.exports = {
   solidity: {
     version: '0.8.10',
 
     networks: {
-      // goerli: {
-      //   url: 'https://rpc.ankr.com/eth_goerli',
+      // rsk: {
+      //   url: `https://rsk.getblock.io/${process.env.GETBLOCK_KEY}/testnet/`,
       //   accounts: [`0x${process.env.PRIVATE_KEY}`],
-      //   // accounts: [`0x${process.env.PRIVATE_KEY}`],
       // },
-      rsk: {
-        url: `https://rsk.getblock.io/${process.env.GETBLOCK_KEY}/testnet/`,
-        accounts: [`0x${process.env.PRIVATE_KEY}`],
-      },
-    },
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200,
-      },
+      localhost: {},
     },
   },
 };
