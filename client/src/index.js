5669359167b5f79360da2b2ecb441f85b8ac2553
skiran017 <skiran017@gmail.com>
2023-07-17 17:37:23 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import { ColorModeScript } from '@chakra-ui/react';
 import React, { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { StateContextProvider } from './context';
 
@@ -12,12 +12,12 @@ const root = ReactDOM.createRoot(container);
 
 root.render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <StateContextProvider>
         <ColorModeScript />
         <App />
       </StateContextProvider>
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>
 );
 
