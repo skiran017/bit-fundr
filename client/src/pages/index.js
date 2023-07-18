bf774df07d20fb5d2b434093946a107ac014f26e
skiran017 <skiran017@gmail.com>
2023-07-18 11:24:45 +0530

    Update index.js

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -2,6 +2,5 @@ export { default as Home } from './Home';
 export { default as About } from './About';
 export { default as Dashboard } from './Dashboard';
 export { default as Explore } from './Explore';
-export { default as NewsLetter } from './NewsLetter';
 export { default as CampaignDetails } from './CampaignDetails';
 export { default as CreateCampaign } from './CreateCampaign';
